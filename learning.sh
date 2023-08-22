45b17a8c7b87c1ab268c67fb6fdf946d3bc41665
tech-vin <tech.vineetsingh@gmail.com>
2023-08-22 08:38:14 +0530

    modified learning.sh

--- learning.sh
+++ learning.sh
@@ -4,3 +4,17 @@
 # Display Hello (Another comment)
 
 echo "Hi there!"
+
+#Assign a value to a variable (first comment)
+# variables are the memory location with name
+# RULES - can contain letters, digits and underscores.
+# cannot start with digit
+# cannot use dashes (-) only underscore can be used as a sepecial character
+# no space 
+WORD='script'
+
+#Display that value using the variable
+echo "$WORD"  # output: script
+echo '$WORD'  # output: WORD
+
+
