f6300df6ac02db6aaa78b9ab52df3d025f19ca4b
tech-vin <tech.vineetsingh@gmail.com>
2023-08-23 20:26:39 +0530

    updated

--- learning.sh
+++ learning.sh
@@ -17,4 +17,26 @@ WORD='script'
 echo "$WORD"  # output: script
 echo '$WORD'  # output: WORD
 
+#Combine the variable with the hardcoded text 
+echo 'This is a shell $WORD'
+echo "This is a shell $WORD"
 
+# Display the contents of the variable using an alternative syntax
+echo "This is a shell ${WORD}"
+
+# Append the text to the variable
+echo "${WORD}ing is fun!!"
+
+# Show how NOT to append text to a variable.
+# This doesn't work:
+echo "$WORDing is fun!!"
+
+# Create a new variable
+ENDING='ed'
+
+# Combine the two variables.
+echo "This is ${WORD}${ENDING}"
+
+#Change the value stored in the ENDING variable. (Reassignment)
+ENDING='ing'
+echo "${WORD}${ENDING} is fun!!"
---
+++ practice.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "first parameter is $1"
+echo "second parameter is $2"
+echo "thoird parameter is $3"
+
+exit 0
